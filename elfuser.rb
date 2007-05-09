a32aeb9ac1d24e173c7c5276dd9600031df11161
Rick Stewart <rick.stewart@theinternetco.net>
2007-05-09 03:17:00 +0000

    Make it work
    darcs-hash:20070509031705-83c56-ccea3aed0adc40f0076de1e5543a15d60543e99f.gz

--- elfuser.rb
+++ elfuser.rb
@@ -12,9 +12,37 @@ module ElfUser
 	end
 
 	module Controllers
+
+		class CreditCardPay < R('/payment')
+			def get
+				@customer = Customer.find(:first, :conditions => ['name = ?', @username])
+				render :creditcardpay
+			end
+			def post
+				@customer = Customer.find(:first, :conditions => ['name = ?', @username])
+				amount = Money.new(BigDecimal.new(@input.amount) * 100)
+				if amount <= @customer.account.balance
+					response = @customer.charge_card(amount)
+					if response.success?
+						redirect R(Index)
+					else
+						raise StandardError, response.message
+					end
+				else
+					@error = 'Amount must be less than your balance'
+					render :error
+				end
+			end
+		end
+
 		class Index < R '/'
 			def get
-				render :index
+				@customer = Customer.find(:first, :conditions => ['name = ?', @username])
+				if !@customer
+					render :nocustomer
+				else
+					render :index
+				end
 			end
 		end
 
@@ -32,8 +60,30 @@ module ElfUser
 	end
 
 	module Views
+		def error
+		end
+
+		def creditcardpay
+			h1 "Charge card"
+			form :action => R(CreditCardPay), :method => 'POST' do
+				p do
+					text "Charge "
+					input :type => 'text', :name => 'amount', :value => @customer.account.balance, :size => 6
+					text " to card *#{@customer.cardnumber[-4..-1]}?"
+				end
+				input :type => 'submit', :value => "Charge"
+			end
+		end
+
 		def index
 			h1 'Customer Account'
+			p @customer.account_name
+			p do 
+				self << "Balance: #{@customer.account.balance.format} "
+				if customer.account.balance > Money.new(0)
+					a('Pay by credit card', :href => R(CreditCardPay))
+				end
+			end
 		end
 
 		def layout
@@ -48,5 +98,34 @@ module ElfUser
 				end
 			end
 		end
+
+		def nocustomer
+			h1 "Not found"
+			p "The user you're logging in as is not in our customer database."
+		end
 	end
 end
+
+class Money
+  def format(*rules)
+    rules = rules.flatten
+
+    if rules.include?(:no_cents)
+      formatted = sprintf("$%d", cents.to_f / 100  )          
+    else
+      formatted = sprintf("$%.2f", cents.to_f / 100  )      
+    end
+
+    if rules.include?(:with_currency)
+      formatted << " "
+      formatted << '<span class="currency">' if rules.include?(:html)
+      formatted << currency
+      formatted << '</span>' if rules.include?(:html)
+    end
+    formatted
+  end
+end
+
+$config = YAML.load_file(File.join(File.dirname(__FILE__), 'db.yaml'))
+
+ActiveRecord::Base.establish_connection(:adapter => 'postgresql', :host => $config['host'], :username => $config['username'], :password => $config['password'], :database => $config['database'])
