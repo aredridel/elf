7d5b6779577727ff46d4b55c2dc6da9101797b98
Aria Stewart <aredridel@nbtsc.org>
2010-12-28 23:58:57 +0100

    Add other fields to javascript transaction editor

--- public/transaction.js
+++ public/transaction.js
@@ -4,10 +4,28 @@ jQuery('document').ready( function() {
 		ev.preventDefault()
 		current = jQuery(this)
 		var n = current.clone()
-		n.find('.debit,.credit').contents().replaceWith(function(i,h) {
+		n.find('.debit,.credit,.number,.memo').contents().replaceWith(function(i) {
 			var t = document.createElement("input")
+			t.setAttribute('name', this.parentNode.getAttribute('class'))
+			t.setAttribute('type', 'text')
 			t.value = this.data.trim()
 			return t
+		})
+		n.find('.date').contents().replaceWith(function(i) {
+			var t = document.createElement('input')
+			t.setAttribute('name', this.parentNode.getAttribute('class'))
+			t.setAttribute('type', 'date')
+			t.value = this.data.trim().replace(/\//g, '-')
+			return t
+		})
+		n.find('.account').contents().replaceWith(function(i) {
+			var t = document.createElement('select')
+			t.setAttribute('name', this.parentNode.getAttribute('class'))
+			var o = document.createElement('option')
+			o.innerText = 'Test'
+			t.appendChild(o)
+			return t
+
 		})
 		current.hide().after(n)
 		n.dblclick(function(ev) {
